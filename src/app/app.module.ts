2fc6108c64140fa20044b5b4852589de2dc8224b
sonjeff <jeffersonbetancourt1@gmail.com>
2021-06-05 14:05:42 -0500

    cambio medio día

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule, NbStepperModule, NbCardModule } from '@nebular/theme';
+import { NbThemeModule, NbLayoutModule, NbStepperModule, NbCardModule, NbTreeGridModule, NbTabsetModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { StepComponent } from './components/step/step.component';
 
 import { NbSidebarModule, NbButtonModule } from '@nebular/theme';
 import { CustomerInformationComponent } from './components/customer-information/customer-information.component';
 import { HousingInformationComponent } from './components/housing-information/housing-information.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatGridListModule, } from '@angular/material/grid-list';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -32,7 +37,8 @@ import { HousingInformationComponent } from './components/housing-information/ho
     NbButtonModule,
     NbStepperModule,
     NbThemeModule.forRoot(),
-    NbCardModule
+    NbCardModule,
+    NbTreeGridModule, NbTabsetModule, MatCardModule, MatToolbarModule, MatButtonModule, FlexLayoutModule, MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
