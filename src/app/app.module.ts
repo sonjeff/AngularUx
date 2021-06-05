4c617245958ec0d3e09a2a4e7eb91063f8dbe572
sonjeff <jeffersonbetancourt1@gmail.com>
2021-06-05 11:46:39 -0500

    cambio error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbThemeModule, NbLayoutModule, NbCardModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { StepComponent } from './components/step/step.component';
 
 import { NbSidebarModule, NbButtonModule } from '@nebular/theme';
 import { CustomerInformationComponent } from './components/customer-information/customer-information.component';
 import { HousingInformationComponent } from './components/housing-information/housing-information.component';
+import {TabPanel, TabViewModule} from 'primeng/tabview';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     StepComponent,
     CustomerInformationComponent,
-    HousingInformationComponent
+    HousingInformationComponent,
+    
   ],
   imports: [
     BrowserModule,
@@ -27,7 +31,9 @@ import { HousingInformationComponent } from './components/housing-information/ho
     NbLayoutModule,
     NbEvaIconsModule,
     NbSidebarModule, 
-    NbButtonModule
+    NbButtonModule,
+    NbCardModule,
+    TabViewModule,TabPanel
   ],
   providers: [],
   bootstrap: [AppComponent]
