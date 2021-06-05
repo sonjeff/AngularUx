3f54f8ae4496db6f1f12c98a3ce08570e5300472
gproano <gonzalo.proano@llacsaa.com>
2021-06-05 10:28:16 -0500

    Crecion de compoentes

---
+++ src/app/components/housing-information/housing-information.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-housing-information',
+  templateUrl: './housing-information.component.html',
+  styleUrls: ['./housing-information.component.css']
+})
+export class HousingInformationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
